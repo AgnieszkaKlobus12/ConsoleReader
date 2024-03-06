37cb4facdf9ade609e76c1c8cf43a0bb736d67a4
Agnieszka Klobus <agnieszka.klobus@qiagen.com>
2024-03-06 11:56:19 +0100

    List files

--- src/main/java/com/console/BookListService.java
+++ src/main/java/com/console/BookListService.java
@@ -1,5 +1,7 @@
 package com.console;
 
+import org.apache.commons.io.FilenameUtils;
+
 import java.io.File;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -7,10 +9,13 @@ import java.util.stream.Stream;
 
 public class BookListService {
 
+    public static final String EPUB_EXTENSION = "epub";
+
     public Set<String> listBookFilesInDirectory(String dir) {
         return Stream.of(new File(dir).listFiles())
                 .filter(file -> !file.isDirectory())
                 .map(File::getName)
+                .filter(name -> FilenameUtils.getExtension(name).equals(EPUB_EXTENSION))
                 .collect(Collectors.toSet());
     }
 
